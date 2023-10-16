4597999b21659bbc1f4315564658c5b92684412b
Guillaume Dhenin <gamecraftprod@gmail.com>
2023-10-16 02:56:40 +0200

    dsqz

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -268,7 +268,7 @@ function upload() {
         if (!imgarrayurl.includes(imageUrl)) {
           imgarrayurl.push(imageUrl);
           
-          for (let i = 0; i < imgarrayurl.length; i++) {
+          for (let i = 0; i < 1; i++) {
             let imgmodal = document.createElement("img");
             let modaldiv = document.createElement("div");
             let spanmodal = document.createElement("span");
