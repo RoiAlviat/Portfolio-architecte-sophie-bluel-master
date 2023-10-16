ca011d96bdbd599e01011740830f858cbc5dac60
Guillaume Dhenin <gamecraftprod@gmail.com>
2023-10-16 02:24:56 +0200

    dqzsdzq

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -275,9 +275,7 @@ function upload() {
           imodal.classList.add("fa-trash-can")
           imodal.classList.add("fa-solid")
     
-          imgmodal.src = travaux[i].imageUrl
-    
-          imgarray.push(travaux[i].imageUrl)
+          imgmodal.src = newimgarray[i].imageUrl
           
           document.querySelector(".modalimgs-container").appendChild(modaldiv)
           modaldiv.appendChild(imgmodal)
